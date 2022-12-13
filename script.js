6c07d9ba3509fac3991f2775ea89f02c8d8b8548
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2022-12-13 21:56:05 +0200

    class1

---
+++ script.js
@@ -0,0 +1,32 @@
+// const message = "JavaScript is awesome!";
+// alert(message);
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// console.log("x > y:", x > y);
+
+// const valueA = "5";
+// console.log(Number(valueA));
+
+
+// const number = "20";
+// console.log(typeof number, number);
+
+// console.log((Number(number)), typeof Number(number));
+
+
+// const validNumber = Number("51");
+// console.log(Number.isNaN(validNumber));
+
+// const age = Number("133");
+// console.log(age);
+// console.log(Number.isNaN(age));
+
